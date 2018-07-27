e084ece81b542cd531c6e610ce0553432c0a5ea1
Melanie Seltzer <melleh11@gmail.com>
2018-07-27 11:43:12 -0700

    :sparkles: Set up the rest of the api endpoints

--- src/controllers/quoteController.js
+++ src/controllers/quoteController.js
@@ -1,13 +1,25 @@
 import { quotes } from '../data/quotes';
 
 exports.getAllQuotes = (req, res) => {
-  res.json(quotes);
+  res.status(200).json(quotes);
 };
 
 exports.getRandomQuote = (req, res) => {
-  res.send('Random quote');
+  const item = quotes[Math.floor(Math.random() * quotes.length)];
+  res.status(200).json(item);
 };
 
 exports.getNumberOfQuotes = (req, res) => {
-  res.send('Get specific number of quotes');
+  // If the number in the URL is larger than the array length
+  // set the count to the array length instead
+  const count = req.params.num > quotes.length ? quotes.length : req.params.num;
+
+  // Make sure we're not mutating the original
+  const newArr = [...quotes];
+
+  // Get n amount of quotes (from count)
+  // after shuffling initial array
+  const items = newArr.sort(() => 0.5 - Math.random()).slice(0, count);
+
+  res.status(200).json(items);
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,13 @@ const express = require('express');
 const router = express.Router();
 const quoteController = require('../controllers/quoteController');
 
+// Get all quotes
 router.get('/quotes', quoteController.getAllQuotes);
+
+// Get a random quote
 router.get('/quotes/random', quoteController.getRandomQuote);
+
+// Get a number of random quotes depending on URL input
 router.get('/quotes/:num', quoteController.getNumberOfQuotes);
 
 module.exports = router;
